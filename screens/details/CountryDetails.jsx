b3cbe46d3886aa8b0ddd9f3e02718763c76ee15a
muhammad-madiha <89494406+madiha-m@users.noreply.github.com>
2023-09-18 16:20:34 +0500

    useRoute hook, got undefined. future task

--- screens/details/CountryDetails.jsx
+++ screens/details/CountryDetails.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { useRoute } from "@react-navigation/native";
 
 const CompanyDetails = () => {
+  const route = useRoute();
+  const { item } = route.params;
+  {
+    console.log(item);
+  }
+
   return (
     <View>
       <Text>CompanyDetails</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CompanyDetails
+export default CompanyDetails;
